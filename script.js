7d6940adc7dbe4adca4733fb8896190b278d5d91
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-07-28 11:47:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,141 @@
 const Questions = [{
     id: 0,
-    question: "What is Homer Simpsons favorite food?",
-    answer: [{ text: "Donuts", isCorrect: true },
-        { text: "Pizza", isCorrect: false },
-        { text: "Hamburgers", isCorrect: false },
-        { text: "Beer", isCorrect: false }
+    q: "What is capital of India?",
+    a: [{ text: "gandhinagar", isCorrect: false },
+        { text: "Surat", isCorrect: false },
+        { text: "Delhi", isCorrect: true },
+        { text: "mumbai", isCorrect: false }
     ]
 
 },
 {
     id: 1,
-    question: "How many colors are in a Rainbow?",
-    answer: [{ text: "Zero", isCorrect: false },
-        { text: "Twenty", isCorrect: false },
-        { text: "Ten", isCorrect: false },
-        { text: "Seven", isCorrect: true }
+    q: "What is the capital of Thailand?",
+    a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+        { text: "phuket", isCorrect: false },
+        { text: "Ayutthaya", isCorrect: false },
+        { text: "Bangkok", isCorrect: true }
     ]
 
 },
 {
     id: 2,
-    question: "Who is the Disney mascot?",
-    answer: [{ text: "Pluto", isCorrect: false },
-        { text: "Mickey Mouse", isCorrect: true },
-        { text: "Donald Duck", isCorrect: false },
-        { text: "Buzz Lightyear", isCorrect: false }
+    q: "What is the capital of Gujarat",
+    a: [{ text: "surat", isCorrect: false },
+        { text: "vadodara", isCorrect: false },
+        { text: "gandhinagar", isCorrect: true },
+        { text: "rajkot", isCorrect: false }
     ]
 
-},
-{
-    id: 3,
-    question: "How many lickes does it take to get to the center of a tootsie pop?",
-    answer: [{ text: "One", isCorrect: false },
-        { text: "Two", isCorrect: false },
-        { text: "Three", isCorrect: false },
-        { text: "The World May NEVER Know", isCorrect: true}
-]
 }
 
 ]
 
+// Set start
 var start = true;
-function iterare(id) {
-var result = document.getElementsByClassName("Answer");
-result[0].innerText ="";
-const question = document.getElementById("Question");
-question.innerText = Questions[id].question;
+
+// Iterate
+function iterate(id) {
+
+// Getting the result display section
+var result = document.getElementsByClassName("result");
+result[0].innerText = "";
+
+// Getting the question
+const question = document.getElementById("question");
 
 
-const Opt1 = document.getElementById('Opt1');
-const Opt2 = document.getElementById('Opt2');
-const Opt3 = document.getElementById('Opt3');
-const Opt4 = document.getElementById('Opt4');
+// Setting the question text
+question.innerText = Questions[id].q;
 
-Opt1.innerText = Questions[id].a[0].text;
-Opt2.innerText = Questions[id].a[1].text;
-Opt3.innerText = Questions[id].a[2].text;
-Opt4.innerText = Questions[id].a[3].text;
+// Getting the options
+const op1 = document.getElementById('op1');
+const op2 = document.getElementById('op2');
+const op3 = document.getElementById('op3');
+const op4 = document.getElementById('op4');
 
-Opt1.value = Questions[id].a[0].isCorrect;
-Opt2.value = Questions[id].a[0].isCorrect;
-Opt3.value = Questions[id].a[0].isCorrect;
-Opt4.value = Questions[id].a[0].isCorrect;
 
-var selected ="";
+// Providing option text
+op1.innerText = Questions[id].a[0].text;
+op2.innerText = Questions[id].a[1].text;
+op3.innerText = Questions[id].a[2].text;
+op4.innerText = Questions[id].a[3].text;
+
+// Providing the true or false value to the options
+op1.value = Questions[id].a[0].isCorrect;
+op2.value = Questions[id].a[1].isCorrect;
+op3.value = Questions[id].a[2].isCorrect;
+op4.value = Questions[id].a[3].isCorrect;
+
+var selected = "";
+
+// Show selection for op1
+op1.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightgoldenrodyellow";
+    op2.style.backgroundColor = "lightskyblue";
+    op3.style.backgroundColor = "lightskyblue";
+    op4.style.backgroundColor = "lightskyblue";
+    selected = op1.value;
+})
+
+// Show selection for op2
+op2.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightskyblue";
+    op2.style.backgroundColor = "lightgoldenrodyellow";
+    op3.style.backgroundColor = "lightskyblue";
+    op4.style.backgroundColor = "lightskyblue";
+    selected = op2.value;
+})
+
+// Show selection for op3
+op3.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightskyblue";
+    op2.style.backgroundColor = "lightskyblue";
+    op3.style.backgroundColor = "lightgoldenrodyellow";
+    op4.style.backgroundColor = "lightskyblue";
+    selected = op3.value;
+})
+
+// Show selection for op4
+op4.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightskyblue";
+    op2.style.backgroundColor = "lightskyblue";
+    op3.style.backgroundColor = "lightskyblue";
+    op4.style.backgroundColor = "lightgoldenrodyellow";
+    selected = op4.value;
+})
+
+// Grabbing the evaluate button
+const evaluate = document.getElementsByClassName("evaluate");
+
+// Evaluate method
+evaluate[0].addEventListener("click", () => {
+    if (selected == "true") {
+        result[0].innerHTML = "True";
+        result[0].style.color = "green";
+    } else {
+        result[0].innerHTML = "False";
+        result[0].style.color = "red";
+    }
+})
+}
+
+if (start) {
+iterate("0");
+}
+
+// Next button and method
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+
+next.addEventListener("click", () => {
+start = false;
+if (id < 2) {
+    id++;
+    iterate(id);
+    console.log(id);
+}
+
+})
+
 

b9062a71f22b6ab52539ba310470f12e23520490
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-07-28 22:41:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,91 @@
-//Your Questions 
-var questionBank= [
-    {
-        question : 'What is Homer Simpsons favorite food',
-        option : ['Donuts','Pizza','Krusty Burger','Beer'],
-        answer : 'Donuts'
-    },
-    {
-        question : 'Who is Disneys mascot',
-        option : ['Pluto','Mickey Mouse','Donold Duck','Daisy Duck'],
-        answer : 'Mickey Mouse'
-    },
-    {
-        question : 'What is the TALLEST building in America',
-        option : ['Sears Tower','Empire State Building','One World Trade Center','The Chrystler Building'],
-        answer : 'One World Trade Center'
-    },
-    {
-        question : 'In Baseball, how many outs are there in an inning',
-        option : ['3','1','6','5'],
-        answer : '3'
-    },
-    {
-        question : 'How many colors are in a rainbow',
-        option : ['10','13','7','4'],
-        answer : '7'
-    },
-]
+//Create a quiz class
+class Quiz {
+    constructor(questions) {
+        this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0;
+    }
 
+    getQuestionIndex() {
+        return this.questions[this.questionIndex];
+    }
+
+    guess(answer) {
+     if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++
+     }
+     this.questionIndex++;
+    }
+
+    isEnded(){
+        return this.questionIndex === this.questions.length;
+    }
+}
+
+//Create your question class
+class Question {
+    constructor(text,choices,answer) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    isCorrectAnswer(choice) {
+        return this.answer === choice;
+    }
+}
+
+//Your Questions 
+function displayQuestion() {
+    if (Quiz.isEnded()) {
+        showScores();
+    } else {
+       // show question
+       let questionElement =document.getElementById("question");
+       questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+       // show options
+       let choices = quiz.getQuestionIndex().choices;
+       for (let i = 0; i < choices.length; i++) {
+            let choiceElement = document.getElementById("choice + i");
+            choiceElement.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+       }
+
+       showProgress();
+    }
+};
+
+//Create Guess Function 
+function guess(id, guess) {
+    let button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        displayQuestion();
+    }
+}
+
+//show quiz progress
+function showProgress() {
+    let currentQuestionNumber = quiz.questionIndex + 1;
+    let progressElement = document.getElementById("progress");
+    progressElement.innerHTML = 
+    'Question ${currentQuestionNumber} of ${quiz.question.length}';
+}
+
+//Show score 
+function showScores() {
+ let quizEndHTML =
+ '
+ <h1>Quiz Completed</h1>
+ <h2 id="score">Your Score: ${quiz.score} of ${quiz.question.length}</h2>
+ <div class="quiz-repeat">
+    <a href="index.html">Take Quiz Again</a>
+
+ </div>
+ ';
+ let quizElement = document.getElementById("quiz");
+ quizElement.innerHTML = quizEndHTML;
+}
+
+//Create your quiz questions 
